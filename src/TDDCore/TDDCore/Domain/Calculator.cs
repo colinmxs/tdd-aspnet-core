e56744525c0929560e83554c001cb0bea0a615bc
colinsmith1 <colinsmith1@boisestate.edu>
2017-02-21 15:50:49 -0700

    created feature tests (RED)

---
+++ src/TDDCore/TDDCore/Domain/Calculator.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TDDCore.Domain
+{
+    public class Calculator
+    {
+        public object Calculate(int v1, int v2)
+        {
+            throw new NotImplementedException();
+        }
+
+        public object Calculate(int v1, int v2, int v3)
+        {
+            throw new NotImplementedException();
+        }
+
+        public object Calculate(int v1, int v2, int v3, int v4)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ tests/TDDCore.Tests/ExampleFeatureTest.cs
@@ -0,0 +1,33 @@
+﻿using Xunit;
+using TDDCore.Domain;
+using Shouldly;
+
+namespace TDDCore.Tests
+{
+    public class ExampleFeatureTest
+    {
+        [Fact]
+        public void SumTwoNumbersTest()
+        {
+            var calculator = new Calculator();
+            var result = calculator.Calculate(2, 2);
+            result.ShouldBe(4);
+        }
+
+        [Fact]
+        public void SumThreeNumbersTest()
+        {
+            var calculator = new Calculator();
+            var result = calculator.Calculate(2, 2, 2);
+            result.ShouldBe(6);
+        }
+
+        [Fact]
+        public void SumFourNumbersTest()
+        {
+            var calculator = new Calculator();
+            var result = calculator.Calculate(2, 2, 2, 3);
+            result.ShouldBe(9);
+        }
+    }
+}
