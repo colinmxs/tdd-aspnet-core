b28f777aa1bfb1a6ec5ea45d50526e64b806c0ea
colinsmith1 <colinsmith1@boisestate.edu>
2017-02-21 15:09:04 -0700

    init

---
+++ src/TDDCore/TDDCore/Startup.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.Mvc.Razor;
+
+namespace TDDCore
+{
+    public class Startup
+    {
+        public Startup(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
+                .AddEnvironmentVariables();
+            Configuration = builder.Build();
+        }
+
+        public IConfigurationRoot Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // Add framework services.
+            services.AddMvc();
+            services.Configure<RazorViewEngineOptions>(options => {
+                options.ViewLocationExpanders.Add(new ViewLocationExpander());
+            });
+        }
+
+        public class ViewLocationExpander : IViewLocationExpander
+        {
+
+            /// <summary>
+            /// Used to specify the locations that the view engine should search to 
+            /// locate views.
+            /// </summary>
+            /// <param name="context"></param>
+            /// <param name="viewLocations"></param>
+            /// <returns></returns>
+            public IEnumerable<string> ExpandViewLocations(ViewLocationExpanderContext context, IEnumerable<string> viewLocations)
+            {
+                //{2} is area, {1} is controller,{0} is the action
+                string[] locations = new string[] 
+                {
+                    "Features/{1}/{0}.cshtml",
+                    "Features/Shared/{0}.cshtml",
+                    "Features/{0}.cshtml"
+                };
+                return locations.Union(viewLocations);          //Add mvc default locations after ours
+            }
+
+
+            public void PopulateValues(ViewLocationExpanderContext context)
+            {
+                context.Values["customviewlocation"] = nameof(ViewLocationExpander);
+            }
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            loggerFactory.AddConsole(Configuration.GetSection("Logging"));
+            loggerFactory.AddDebug();
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+
+                // Browser Link is not compatible with Kestrel 1.1.0
+                // For details on enabling Browser Link, see https://go.microsoft.com/fwlink/?linkid=840936
+                // app.UseBrowserLink();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+
+            app.UseStaticFiles();
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
