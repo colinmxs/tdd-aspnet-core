339ff3e83cc4973c3db2aefaf842ba9da62d4729
colinsmith1 <colinsmith1@boisestate.edu>
2017-02-21 15:14:07 -0700

    added xunit project

---
+++ tests/TDDCore.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Xunit;
+
+namespace TDDCore.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.True(1 + 1 == 2);
+        }
+    }
+}
