5700857909766158e2cd6932eea80b9165b1e399
colinsmith1 <colinsmith1@boisestate.edu>
2017-02-22 13:19:24 -0700

    (REFACTOR) made the code dry

--- src/TDDCore/TDDCore/Features/Calculator/Sum.cs
+++ src/TDDCore/TDDCore/Features/Calculator/Sum.cs
@@ -7,38 +7,24 @@ namespace TDDCore.Features.Calculator
     {
         public class Query : IRequest<int>
         {
-            private int v1;
-            private int v2;
-            private int v3;
-            private int v4;
+            internal int[] _ints;
 
-            public Query(int v1, int v2)
+            public Query(params int[] ints)
             {
-                this.v1 = v1;
-                this.v2 = v2;
-            }
-
-            public Query(int v1, int v2, int v3)
-            {
-                this.v1 = v1;
-                this.v2 = v2;
-                this.v3 = v3;
-            }
-
-            public Query(int v1, int v2, int v3, int v4)
-            {
-                this.v1 = v1;
-                this.v2 = v2;
-                this.v3 = v3;
-                this.v4 = v4;
+                _ints = ints;
             }
         }
 
         public class QueryHandler : IAsyncRequestHandler<Query, int>
         {
-            public Task<int> Handle(Query message)
+            public async Task<int> Handle(Query message)
             {
-                throw new NotImplementedException();
+                var sum = 0;
+                foreach (var @int in message._ints)
+                {
+                    sum = sum + @int;
+                }
+                return sum;
             }
         }
     }
