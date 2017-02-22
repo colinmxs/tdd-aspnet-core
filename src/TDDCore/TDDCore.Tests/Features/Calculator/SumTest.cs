788f3b91e648c02816b876e5bb020ed6687faa5b
colinsmith1 <colinsmith1@boisestate.edu>
2017-02-22 16:27:27 -0700

    (RED)

---
+++ src/TDDCore/TDDCore.Tests/Features/Calculator/SumTest.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Shouldly;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using TDDCore.Features.Calculator;
+using static TDDCore.Tests.TestFixture;
+
+namespace TDDCore.Tests.Features.Calculator
+{
+    [TestClass]
+    public class SumTest
+    {
+        [TestMethod]
+        public async Task Sum3Number()
+        {
+            var query = new Sum.Query(2, 2);
+            var result = await SendAsync(query);
+            result.ShouldBe(4);
+        }
+        [TestMethod]
+        public async Task Sum2Number()
+        {
+            var query = new Sum.Query(2, 3, 9);
+            var result = await SendAsync(query);
+            result.ShouldBe(14);
+        }
+        [TestMethod]
+        public async Task Sum4Number()
+        {
+            var query = new Sum.Query(4,5,21,19);
+            var result = await SendAsync(query);
+            result.ShouldBe(49);
+        }
+    }
+}
--- src/TDDCore/TDDCore/Features/Calculator/Sum.cs
+++ src/TDDCore/TDDCore/Features/Calculator/Sum.cs
@@ -1,30 +1,47 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using MediatR;
+
 namespace TDDCore.Features.Calculator
 {
     public class Sum
     {
         public class Query : IRequest<int>
         {
-            internal int[] _ints;
+            private int v1;
+            private int v2;
+            private int v3;
+            private int v4;
 
-            public Query(params int[] ints)
+            public Query(int v1, int v2)
             {
-                _ints = ints;
+                this.v1 = v1;
+                this.v2 = v2;
+            }
+
+            public Query(int v1, int v2, int v3)
+            {
+                this.v1 = v1;
+                this.v2 = v2;
+                this.v3 = v3;
+            }
+
+            public Query(int v1, int v2, int v3, int v4)
+            {
+                this.v1 = v1;
+                this.v2 = v2;
+                this.v3 = v3;
+                this.v4 = v4;
             }
         }
 
         public class QueryHandler : IAsyncRequestHandler<Query, int>
         {
-            public async Task<int> Handle(Query message)
+            public Task<int> Handle(Query message)
             {
-                var sum = 0;
-                foreach (var @int in message._ints)
-                {
-                    sum = sum + @int;
-                }
-                return sum;
+                throw new NotImplementedException();
             }
         }
     }
